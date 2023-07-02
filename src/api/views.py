d08936fbd281e5899ec7814ce9159a9a35d19d6f
Sergey <ssnovak@stud.kpfu.ru>
2023-07-02 22:23:08 +0300

    + api search, role and adeed delete in api

--- src/api/views.py
+++ src/api/views.py
@@ -4,17 +4,31 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.renderers import JSONRenderer
 
+from django.db.models import Q
+
 from .serializers import UserSerializer
 from users.models import User
 
 
 class UserView(APIView):
-    permission_classes = (AllowAny, )
+    permission_classes = (AllowAny,)
     serializer_class = UserSerializer
     renderer_classes = (JSONRenderer,)
 
     def get(self, request, id=None, *args, **kwargs):
-        queryset = User.objects.filter(id=id) if id else User.objects.all()
+        role_filter = request.GET.get('role')
+        search_query = request.GET.get('search')
+        queryset = User.objects.all()
+
+        if search_query:
+            queryset = queryset.filter(Q(username__icontains=search_query) | Q(email__icontains=search_query))
+
+        if role_filter:
+            queryset = queryset.filter(role=role_filter)
+
+        if id is not None:
+            queryset = queryset.filter(id=id)
+
         serializer = UserSerializer(queryset, many=True)
         return Response(serializer.data)
 
@@ -33,3 +47,9 @@ class UserView(APIView):
         serializer.is_valid(raise_exception=True)
         updated_user_obj = serializer.save()
         return Response(UserSerializer(updated_user_obj).data, status=status.HTTP_200_OK)
+
+    def delete(self, request, *args, **kwargs):
+        id = self.kwargs["id"]
+        queryset = User.objects.filter(id=id)
+        queryset.delete()
+        return Response(status=status.HTTP_200_OK)
